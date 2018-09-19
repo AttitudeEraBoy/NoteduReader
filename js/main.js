b9b11b34b7beabf218ef81050a964e922aa3898b
İren SALTALI <iren@saltali.com>
2018-09-19 22:52:25 +0300

    First popover is created

--- js/main.js
+++ js/main.js
@@ -65,26 +65,17 @@ $(document).ready(function () {
 
     });
     
-    $('.pop').popover({
-        trigger: 'focus'
-      })
 
-    // $(".pop").popover({ trigger: "manual" , html: true, animation:false})
-    // .on("mouseenter", function () {
-    //     var _this = this;
-    //     $(this).popover("show");
-    //     $(".popover").on("mouseleave", function () {
-    //         $(_this).popover('hide');
-    //     });
-    // }).on("mouseleave", function () {
-    //     var _this = this;
-    //     setTimeout(function () {
-    //         if (!$(".popover:hover").length) {
-    //             $(_this).popover("hide");
-    //         }
-    //     }, 300);
-    // });
-
+    //Popovers
+    $('#dos').popover({
+        trigger: 'focus',
+        html:true,
+        title:'test sdffsadfsa',
+        content:'sdfsfas safas sf',
+        placement:'right',
+        boundary:'window',
+        template:'<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+      }); 
     
 });
 
